1c1b9c85bada52a7e36c86c44ccfe8bde5e9cf93
jalalseidi <jalalseidi7@gmail.com>
2025-02-10 19:47:47 +0300

    Adding UI to the QuizGame

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+from ui import QuizInterface
 
 question_bank = []
 for question in question_data:
@@ -11,10 +12,10 @@ for question in question_data:
 
 
 quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface(quiz)
 
-while quiz.still_has_question():
-    quiz.next_question()
+# while quiz.still_has_questions():
+#     quiz.next_question()
 
-print("You've completed the quiz.")
-print(f"You final score was: {quiz.score}/{quiz.question_number}")
-
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
